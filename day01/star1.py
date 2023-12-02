5b47c52463d1972eef4da53da5d405e19a735412
Milo Politan <milobe.115@gmail.com>
2023-12-02 09:28:53 +0100

    Cleanup on day01

--- day01/star1.py
+++ day01/star1.py
@@ -1,12 +1,14 @@
 import re
 
-f = open("./input.txt", "r")
-lines = f.readlines()
 
-values = []
+def main():
+    with open("./input.txt", "r") as f:
+        lines = f.readlines()
+    values = []
+    for line in lines:
+        numbers = re.findall(r'(\d)', line)
+        values.append(int(numbers[0] + numbers[-1]))
+    print(f"Calibration sum : {sum(values)}")
 
-for line in lines:
-    numbers = re.findall(r'(\d)', line)
-    values.append(int(numbers[0] + numbers[-1]))
-
-print(f"Calibration sum : {sum(values)}")
+if __name__ == "__main__":
+    main()
